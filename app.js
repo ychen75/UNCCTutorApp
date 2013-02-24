ac6fcb533b55c72feb10398976c5cdd067741961
Austin Beeler <travbeele@gmail.com>
2013-02-24 05:12:03 -0500

    Schemas and basic layout
    Added in the schemas and models for the three collections we will be
    using. Also created a very simple layout following the mockup's design.
    Needs to be fleshed out and have the tray added.

--- app.js
+++ app.js
@@ -31,24 +31,55 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-//define new routes here
-app.get('/', routes.index);
-app.get('/users', user.list);
-app.get('/about', routes.about);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
-
-mongoose.connect('mongodb://localhost/test');
+// Connect to mongodb databases stored on the localhost and use the database named 'test'
+mongoose.connect('localhost','test');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function callback() {
-//define schema
-    var UserSchema = new Schema({
-      name:     String,
-      isTutor:  Boolean,
-      messages:  String
-   });
+  // Anything that needs to be run each time the server is started
+  console.log('Database connection: OK');
+});
 
+// SCHEMAS
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+var userSchema = new mongoose.Schema({
+  name: String,
+  isTutor: Boolean,
+  messages: [ObjectId],
+  schedule: [ObjectId]
+});
+var messageSchema = new mongoose.Schema({
+  subscribers: [Number],
+  posts: [{
+    posterId: ObjectId,
+    postText: String,
+    timePosted: Date
+  }],
+});
+var eventSchema = new mongoose.Schema({
+  ownerID: ObjectId,
+  timeOf: Date,
+  location: String,
+  title: String,
+  maxRegistrants: Number,
+  registrantID: [Number]
+});
+
+// MODELS
+var User = mongoose.model('User', userSchema);
+var Message = mongoose.model('Message', messageSchema);
+var Event = mongoose.model('Event', eventSchema);
+
+// ROUTES
+// Index
+app.get('/', function(req, res) {
+  res.render('index', {
+    title: 'UNCC Tutor Tracker'
+  });
+});
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
 });
